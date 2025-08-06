b1d03428b8baeaae165964d5eec4cabf36910120
habi802 <shb8002@gmail.com>
2025-08-06 15:56:13 +0900

    [feat] userSignUpReq 유효성 검사 추가

--- src/main/java/kr/co/greengram/application/user/UserController.java
+++ src/main/java/kr/co/greengram/application/user/UserController.java
@@ -1,9 +1,15 @@
 package kr.co.greengram.application.user;
 
+import jakarta.validation.Valid;
+import kr.co.greengram.application.user.model.UserSignUpReq;
+import kr.co.greengram.config.model.ResultResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -11,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/sign-up")
+    public ResultResponse<?> signUp(@Valid @RequestPart UserSignUpReq req,
+                                    @RequestPart(required = false) MultipartFile pic) {
+        log.info("req: {}", req);
+        log.info("pic: {}", pic);
+        return new ResultResponse<Integer>("", 1);
+    }
 }
