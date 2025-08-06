e557eec77cd9568c8e793c83c238804f2062d6c5
habi802 <shb8002@gmail.com>
2025-08-06 17:10:04 +0900

    [refactor] 유효성 검사 예외 처리 부분 수정

--- src/main/java/kr/co/greengram/config/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/greengram/config/exception/GlobalExceptionHandler.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -43,6 +44,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private List<ValidationError> getValidationError(BindException e) {
         List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
-        return fieldErrors.stream().map(item -> ValidationError.of(item)).toList();
+        //return fieldErrors.stream().map(item -> ValidationError.of(item)).toList();
+        List<ValidationError> result = new ArrayList<>(fieldErrors.size());
+        for (FieldError fieldError : fieldErrors) {
+            result.add(ValidationError.of(fieldError));
+        }
+        return result;
     }
 }
