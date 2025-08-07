b1b2fd9fea70c83be8b421cba44b1780a7f96c0b
habi802 <shb8002@gmail.com>
2025-08-07 11:11:48 +0900

    [feat] 회원 가입 부분 이미지 추가

--- src/main/java/kr/co/greengram/application/user/UserService.java
+++ src/main/java/kr/co/greengram/application/user/UserService.java
@@ -1,11 +1,13 @@
 package kr.co.greengram.application.user;
 
 import kr.co.greengram.application.user.model.UserSignUpReq;
+import kr.co.greengram.config.util.ImgUploadManager;
 import kr.co.greengram.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -14,7 +16,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ImgUploadManager imgUploadManager;
 
+    @Transactional
     public void signUp(UserSignUpReq req, MultipartFile pic) {
         String hashedPassword = passwordEncoder.encode(req.getUpw());
 
@@ -25,5 +29,10 @@ public class UserService {
         user.addUserRoles(req.getRoles());
 
         userRepository.save(user);
+
+        if (pic != null) {
+            String savedFileName = imgUploadManager.saveProfilePic(user.getUserId(), pic);
+            user.setPic(savedFileName);
+        }
     }
 }
