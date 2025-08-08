341c76a7587ebfbb388cb5433433264104fef48f
habi802 <shb8002@gmail.com>
2025-08-08 12:40:51 +0900

    [feat] feed 엔터티 생성

---
+++ src/main/java/kr/co/greengram/entity/Feed.java
@@ -0,0 +1,26 @@
+package kr.co.greengram.entity;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode
+public class Feed extends UpdatedAt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long feedId;
+
+    @Column(length = 30)
+    private String location;
+
+    @Column(length = 1000)
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "write_user_id", nullable = false)
+    private User user;
+}
