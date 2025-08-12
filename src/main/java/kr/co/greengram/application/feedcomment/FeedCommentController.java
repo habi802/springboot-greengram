a80113242dd6efaeeb719faf62328744ee82e391
habi802 <shb8002@gmail.com>
2025-08-12 13:17:18 +0900

    [feat] feed comment 등록 추가

--- src/main/java/kr/co/greengram/application/feedcomment/FeedCommentController.java
+++ src/main/java/kr/co/greengram/application/feedcomment/FeedCommentController.java
@@ -1,7 +1,15 @@
 package kr.co.greengram.application.feedcomment;
 
+import jakarta.validation.Valid;
+import kr.co.greengram.application.feedcomment.model.FeedCommentPostReq;
+import kr.co.greengram.config.model.ResultResponse;
+import kr.co.greengram.config.model.UserPrincipal;
+import kr.co.greengram.entity.FeedComment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FeedCommentController {
     private final FeedCommentService feedCommentService;
+
+    @PostMapping
+    public ResultResponse<?> postFeedComment(@AuthenticationPrincipal UserPrincipal userPrincipal,
+                                             @Valid @RequestBody FeedCommentPostReq req) {
+        log.info("signedUserId: {}", userPrincipal.getSignedUserId());
+        log.info("post feed comment req: {}", req);
+        return null;
+    }
 }
