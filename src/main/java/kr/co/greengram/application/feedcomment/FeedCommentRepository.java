b9f48e9b1b24755034d45f1fa83b0cdbab0bd653
habi802 <shb8002@gmail.com>
2025-08-12 12:59:48 +0900

    [feat] feed comment 리포지토리, 서비스, 컨트롤러 추가

---
+++ src/main/java/kr/co/greengram/application/feedcomment/FeedCommentRepository.java
@@ -0,0 +1,7 @@
+package kr.co.greengram.application.feedcomment;
+
+import kr.co.greengram.entity.FeedComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedCommentRepository extends JpaRepository<FeedComment, Long> {
+}
