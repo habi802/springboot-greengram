2a6132077b5d0a902af521117859f987d3657042
habi802 <shb8002@gmail.com>
2025-08-13 10:28:46 +0900

    [feat] get feed comment list 추가

--- src/main/java/kr/co/greengram/application/feedcomment/FeedCommentController.java
+++ src/main/java/kr/co/greengram/application/feedcomment/FeedCommentController.java
@@ -1,16 +1,14 @@
 package kr.co.greengram.application.feedcomment;
 
 import jakarta.validation.Valid;
+import kr.co.greengram.application.feedcomment.model.FeedCommentGetReq;
 import kr.co.greengram.application.feedcomment.model.FeedCommentPostReq;
 import kr.co.greengram.config.model.ResultResponse;
 import kr.co.greengram.config.model.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -26,5 +24,11 @@ public class FeedCommentController {
         log.info("post feed comment req: {}", req);
         long feedCommentId = feedCommentService.postFeedComment(userPrincipal.getSignedUserId(), req);
         return new ResultResponse<>("댓글 등록 성공!", feedCommentId);
+    }
+
+    @GetMapping
+    public ResultResponse<?> getFeedCommentList(@Valid @ModelAttribute FeedCommentGetReq req) {
+        log.info("get feed comment req: {}", req);
+        return null;
     }
 }
